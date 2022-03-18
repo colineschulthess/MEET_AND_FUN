5980264d23525fddd64271d25bc3ef3f6cc1440f
dylan dol <dylan.dol@orange.fr>
2022-03-18 13:32:08 -0400

    new nav and size logo décrémentation

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -33,7 +33,7 @@ class ExperiencesController < ApplicationController
   def show
     @experiences_lastminute = Experience.where("date > ?", Date.today).order("date ASC").limit(6)
     @capacity = @experience.capacity
-    #@actual_capacity = @capacity - @experience.booking.count
+    @actual_capacity = @capacity - @experience.bookings.count
   end
 
   def edit
